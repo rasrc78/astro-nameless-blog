192ec1fdcab8c24d148422f1d56e8f95db8e71ff
Sh2-103 <Sh2-103@outlook.com>
2026-08-09 01:42:35 +0800

    feat: add github action config used for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://localhost:4321',
+    site: 'https://uchuu.top',
+    base: '/',
 });
